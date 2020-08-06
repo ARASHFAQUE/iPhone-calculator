bc59325d285b0d31d79e49c35c2afd2a59fcf0e4
ARASHFAQUE <55973326+ARASHFAQUE@users.noreply.github.com>
2020-08-06 23:23:08 +0600

    iphone calculator done

---
+++ script.js
@@ -0,0 +1,22 @@
+const inputValue = document.getElementById('result');
+function inputNumbers(num){
+    document.getElementById('delete').value = 'C';
+    const existingNum = inputValue.value;
+    let totalInputValue = existingNum + num;
+    inputValue.value = totalInputValue;
+    return totalInputValue;
+}
+function inputNegativeNumbers(){
+    document.getElementById('delete').value = 'C';
+    let valueInput = inputValue.value;
+    const negativeValue = valueInput * -1;
+    inputValue.value = negativeValue;
+}
+
+function deleteAll(){
+    inputValue.value = "";
+}
+function calculateAll(){
+    const result = eval(inputValue.value);
+    inputValue.value = result;
+}
